bc68c0b0fab5323d36d651fee92b8b845f4a2488
Alyssa Marie Delgado <alyssamariedelgado3@gmail.com>
2026-05-21 16:18:44 +0800

    swagger.yaml

--- _helpers/swagger.ts
+++ _helpers/swagger.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
+import fs from 'fs';
 import path from 'path';
 
 const router = express.Router();
 
-// Load swagger.yaml from the project root in both source and built dist modes
-const swaggerDocument = YAML.load(path.join(__dirname, '..', 'swagger.yaml'));
+const distSwaggerPath = path.join(__dirname, '..', 'swagger.yaml');
+const projectSwaggerPath = path.join(process.cwd(), 'swagger.yaml');
+const swaggerPath = fs.existsSync(distSwaggerPath) ? distSwaggerPath : projectSwaggerPath;
+
+const swaggerDocument = YAML.load(swaggerPath);
 
 router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
