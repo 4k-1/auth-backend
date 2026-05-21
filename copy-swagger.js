7e43b678c7ce747adacac05776e68d454fb58b74
Alyssa Marie Delgado <alyssamariedelgado3@gmail.com>
2026-05-21 15:35:10 +0800

    lassttt

--- copy-swagger.js
+++ copy-swagger.js
@@ -1,13 +1,11 @@
-// copy-assets.js - Works on Windows, Mac, Linux
 const fs = require('fs');
 const path = require('path');
 
-const filesToCopy = ['.env', 'config.json'];
+const filesToCopy = ['.env', 'config.json', 'swagger.yaml']; 
 
 filesToCopy.forEach(file => {
   const src = path.join(__dirname, file);
   const dest = path.join(__dirname, 'dist', file);
-  
   if (fs.existsSync(src)) {
     fs.copyFileSync(src, dest);
     console.log(`✅ Copied ${file} to dist/`);
